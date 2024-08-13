bbe9d9d61ff63c0b16037b2db5e66c365f61e20c
Your Name <you@example.com>
2024-08-13 09:17:02 +0100

    fixing Unauthenticated

--- app/Http/Controllers/Api/v1/CheckoutController.php
+++ app/Http/Controllers/Api/v1/CheckoutController.php
@@ -395,7 +395,7 @@ class CheckoutController extends FrontendController
                 [
                     'amount' => [
                         'currency_code' => 'USD',
-                        'value' => $totalAmount,
+                        'value' => 100,
                     ],
                 ],
             ],
