75f58004302f9c14b08f7e091dde102bc5f75224
Your Name <you@example.com>
2024-08-23 11:25:57 +0100

    about, terms and privacy

--- resources/views/admin/restaurant/index.blade.php
+++ resources/views/admin/restaurant/index.blade.php
@@ -20,14 +20,50 @@
                     </div>
                     @endcan
 
-                    <div class="card-body">
-                        <div class="row">
-                            <form>
-                                <textarea> Hello</textarea>
-                            </form>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-sm-6 offset-sm-3">
+                                    <div class="input-group input-daterange" id="date-picker">
+                                        <select class="form-control" id="status" name="status">
+                                            @foreach (trans('restaurant_statuses') as $key => $status)
+                                                <option value="{{ $key }}">{{ $status }}</option>
+                                            @endforeach
+                                        </select>
+                                        <select class="form-control" id="applied" name="applied">
+                                            <option value="">{{ __('levels.select_applied') }}</option>
+                                            @foreach (trans('restaurant_applieds') as $key => $restaurant_applied)
+                                                <option value="{{ $key }}">{{ $restaurant_applied }}</option>
+                                            @endforeach
+                                        </select>
+                                        <div class="input-group-append">
+                                            <button class="btn btn-outline-secondary" type="button" id="refresh">
+                                                {{ __('levels.refresh') }}</button>
+                                        </div>
+                                        <div class="input-group-append">
+                                            <button class="btn btn-outline-secondary" type="button" id="date-search">{{
+                                            __('levels.search') }}</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <br>
+                            <div class="table-responsive">
+                                <table class="table table-striped" id="maintable"
+                                       data-url="{{ route('admin.restaurant.get-restaurant') }}"
+                                       data-status="{{ \App\Enums\RestaurantStatus::ACTIVE }}"
+                                       data-hidecolumn="{{ auth()->user()->can('restaurants_show') ||auth()->user()->can('restaurants_edit') ||auth()->user()->can('restaurants_delete') }}">
+                                    <thead>
+                                    <tr>
+                                        <th>{{ __('levels.id') }}</th>
+                                        <th>{{ __('levels.name') }}</th>
+                                        <th>{{ __('levels.user') }}</th>
+                                        <th>{{ __('levels.status') }}</th>
+                                        <th>{{ __('levels.actions') }}</th>
+                                    </tr>
+                                    </thead>
+                                </table>
+                            </div>
                         </div>
-                        <br>
-                    </div>
                 </div>
             </div>
         </div>
